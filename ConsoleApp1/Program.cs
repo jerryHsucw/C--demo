180f9e46a0505edb143babac0808cf1fe3a8395f
jerryHsucw <dreawn@hotmail.com>
2025-10-22 09:00:54 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Intrinsics.Arm;
+
+Console.WriteLine("Hello, World!");
+
+int i = 0;
+short sh = 0;
+long lo = 0;
+Console.WriteLine("i="+i);
+Console.WriteLine("i = {0} , x = {1}", i , "string");
+
+double d = 0;
+double dd = 0d;
+
+float f = 0;
+float ff = 0f;
+
+decimal dm = 0;
+decimal dm2 = 0m;
+
+Console.WriteLine(d);
+Console.WriteLine(dd);
